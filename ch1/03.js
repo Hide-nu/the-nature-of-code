b9fc7d3e2c31ff2ea8ffcaecce2c93d4bb79b324
Hide-nu <nkret805worm@gmail.com>
2022-04-29 19:10:50 +0900

    add vector math

---
+++ ch1/03.js
@@ -0,0 +1,22 @@
+/// <reference path='../TSDef/p5.global-mode.d.ts' />
+
+"use strict;";
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(255);
+  strokeWeight(2);
+  stroke(0);
+  noFill();
+
+  translate(width / 2, height / 2);
+  ellipse(0, 0, 4, 4);
+
+  const mouse = createVector(mouseX, mouseY);
+  const center = createVector(width / 2, height / 2);
+
+  mouse.sub(center);
+  line(0, 0, mouse.x, mouse.y);
+}
