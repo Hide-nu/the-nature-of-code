721ed3b446e4693ebc17d8a704cb761bba8f566e
Hide-nu <nkret805worm@gmail.com>
2022-05-02 00:17:12 +0900

    mutual attraction

--- ch2/04.js
+++ ch2/04.js
@@ -2,37 +2,50 @@
 
 "use strict;";
 
-let m;
+let movers = [];
 function setup() {
   createCanvas(800, 600);
-  m = new Mover();
+  for (let i = 0; i < 2; i++) {
+    const m = new Mover(random(0, 1, 2), random(width), random(height));
+    movers.push(m);
+  }
 }
 
 function draw() {
   background(255);
-  const g = createVector(0, 0.2);
-  g.mult(m.mass);
-  m.applyForce(g);
-  // const wind = createVector(0.3, 0);
-  // m.applyForce(wind);
-  const friction = m.velocity.copy();
-  friction.normalize();
-  const c = -0.01;
-  const speed = m.velocity.mag();
-  friction.mult(c * speed * speed);
-  m.applyForce(friction);
 
-  m.update();
-  m.edges();
-  m.display();
+  for (let i = 0; i < movers.length; i++) {
+    for (let j = 0; j < movers.length; j++) {
+      if (i != j) {
+        const force = movers[j].attract(movers[i]);
+        movers[i].applyForce(force);
+      }
+    }
+
+    movers[i].update();
+    // m.edges();
+    movers[i].display();
+  }
+
+  // const g = createVector(0, 0.2);
+  // g.mult(m.mass);
+  // m.applyForce(g);
+  // // const wind = createVector(0.3, 0);
+  // // m.applyForce(wind);
+  // const friction = m.velocity.copy();
+  // friction.normalize();
+  // const c = -0.01;
+  // const speed = m.velocity.mag();
+  // friction.mult(c * speed * speed);
+  // m.applyForce(friction);
 }
 
 class Mover {
-  constructor() {
-    this.location = createVector(width / 2, height / 2);
+  constructor(m, x, y) {
+    this.location = createVector(x, y);
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
-    this.mass = 1;
+    this.mass = m;
   }
 
   update() {
@@ -50,7 +63,7 @@ class Mover {
     stroke(0);
     strokeWeight(2);
     fill(127);
-    ellipse(this.location.x, this.location.y, 48, 48);
+    ellipse(this.location.x, this.location.y, this.mass * 24, this.mass * 24);
   }
 
   edges() {
@@ -75,5 +88,15 @@ class Mover {
   applyForce(force) {
     const f = force.div(this.mass);
     this.acceleration.add(f);
+  }
+
+  attract(m, g) {
+    const force = this.location.sub(m.location);
+    const distance = force.mag();
+    force.normalize();
+
+    const strength = (g * this.mass * m.mass) / (distance * distance);
+    force.mult(strength);
+    return force;
   }
 }
